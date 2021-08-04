be30dbb9aeacd6f8244e3db5b960c221dcc9ffd4
Cody Shepherd <cody.shepherd@gmail.com>
2021-08-04 09:09:25 -0700

    fix: handle https in systemd wrapper, and prevent it from looping forever

--- scripts/influxd-systemd-start.sh
+++ scripts/influxd-systemd-start.sh
@@ -1,17 +1,32 @@
 #!/bin/bash -e
 
 /usr/bin/influxd &
-echo $! > /var/lib/influxdb/influxd.pid
+PID=$!
+echo $PID > /var/lib/influxdb/influxd.pid
 
+PROTOCOL="http"
 BIND_ADDRESS=$(influxd print-config --key-name http-bind-address)
+TLS_CERT=$(influxd print-config --key-name tls-cert)
+TLS_KEY=$(influxd print-config --key-name tls-key)
+if [ -n "${TLS_CERT}" ] && [ -n "${TLS_KEY}" ]; then
+  echo "TLS cert and key found -- using https"
+  PROTOCOL="https"
+fi
 HOST=${BIND_ADDRESS%%:*}
 HOST=${HOST:-"localhost"}
 PORT=${BIND_ADDRESS##*:}
 
 set +e
-result=$(curl -s -o /dev/null http://$HOST:$PORT/ready -w %{http_code})
+max_attempts=15
+url="$PROTOCOL://$HOST:$PORT/ready"
+result=$(curl -k -s -o /dev/null $url -w %{http_code})
 while [ "$result" != "200" ]; do
   sleep 1
-  result=$(curl -s -o /dev/null http://$HOST:$PORT/ready -w %{http_code})
+  result=$(curl -k -s -o /dev/null $url -w %{http_code})
+  max_attempts=$(($max_attempts-1))
+  if [ $max_attempts -le 0 ]; then
+    echo "Failed to reach influxdb $PROTOCOL endpoint at $url"
+    exit 1
+  fi
 done
 set -e
